d68a8a90a6b4e8ccb94d2b935aaf030ea733b3fd
nourhan36 <nourhanibrahim2004@gmail.com>
2023-12-29 00:23:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/courseapp/MainCoursesPage.java
@@ -0,0 +1,15 @@
+package com.example.courseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainCoursesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_courses_page);
+
+    }
+}
