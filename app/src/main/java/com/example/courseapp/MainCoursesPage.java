8e81a2c5237321fc29bf88cffc71c2df4b52ac27
nourhan36 <nourhanibrahim2004@gmail.com>
2023-12-29 00:48:40 +0200

    initial commit

--- app/src/main/java/com/example/courseapp/MainCoursesPage.java
+++ app/src/main/java/com/example/courseapp/MainCoursesPage.java
@@ -2,14 +2,19 @@ package com.example.courseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainCoursesPage extends AppCompatActivity {
 
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_courses_page);
-
+        setContentView(R.layout.activity_course_page);
+        txt = findViewById(R.id.tv_show);
+        txt.setMovementMethod(new ScrollingMovementMethod());
     }
 }
